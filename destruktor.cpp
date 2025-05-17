2008f5fb2f73a671d6baca4d495a4b17fba9af31
rachelnova18 <rachelnv26@gmail.com>
2025-05-17 16:15:59 +0700

    membuat fungsi untuk isi data

--- destruktor.cpp
+++ destruktor.cpp
@@ -37,3 +37,13 @@ void angka::cetakData()
     }
 }
 
+void angka::isiData()
+{
+    for (int i = 1; i <= panjang; i++)
+    {
+        cout << i << " = ";
+        cin >> arr[i];
+    }
+    cout << endl;
+}
+
