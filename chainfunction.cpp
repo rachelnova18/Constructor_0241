a55ba19285d8d2c53c7f3a44e7a020b8c389afd1
rachelnova18 <rachelnv26@gmail.com>
2025-05-17 15:59:45 +0700

    membuat akses modifier

--- chainfunction.cpp
+++ chainfunction.cpp
@@ -2,3 +2,18 @@
 using namespace std;
 
 class buku
+{
+    string judul;
+
+public:
+    buku setJudul(string judul)
+    {
+        this->judul = judul;
+        return *this; // chain function
+    }
+    string getJudul()
+    {
+        return this->judul;
+    }
+} bukunya;
+
