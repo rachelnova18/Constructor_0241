c72fce7956cf32b9ee02b6d2faa5e23b3036a4b1
rachelnova18 <rachelnv26@gmail.com>
2025-05-17 16:25:15 +0700

    add fungsi untuk cetak

--- konstruktor.cpp
+++ konstruktor.cpp
@@ -37,3 +37,10 @@ mahasiswa::mahasiswa(int iNim, string iNama)
     nim = iNim;
     nama = iNama;
 }
+void mahasiswa::cetak()
+{
+    cout << endl
+         << "Nim = " << nim << endl;
+    cout << "Nama = " << nama << endl;
+}
+
